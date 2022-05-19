e013552a07ca950c78419b0a5b0d702fc09948df
Farid T. Baliyev <faridtb@code.local>
2022-05-19 19:06:28 +0400

    feat:Class Time

---
+++ index.js
@@ -0,0 +1,66 @@
+
+//#region GetMax from array
+
+// function GetMax(arr)
+// {
+//     var maxElem = arr[0];
+//     for (let i = 0; i < arr.length; i++)
+//     {
+//       if(arr[i]>maxElem)
+//       {
+//           maxElem=arr[i];
+//       }
+//     }
+//     return maxElem;
+// }
+
+// console.log(GetMax([3,12,5,0]));
+
+//#endregion
+
+//#region  Delete from Array
+// function DelSelect(arr,num)
+// {
+//     for (let i = 0; i < arr.length; i++) {
+//         if(num==arr[i])
+//         {
+//             arr[i]=arr[arr.length-1];
+//             arr.length-=1;
+//         }
+//     }
+
+//     for (let i=0; i < arr.length; i++)
+//     {
+//         for (let j = 0; j < arr.length; j++) 
+//         {
+//             if(arr[i]<arr[j])
+//         {
+//             arrMin=arr[j];
+//             arr[j]=arr[i];
+//             arr[i]=arrMin;
+//         }
+          
+//         }
+
+//     }
+//     return arr;
+// }
+
+// console.log(DelSelect([2,3,4,5,6,7],6 ));
+
+//#endregion
+
+//#region  Add +5 to Indexes of Array
+// function GetFive(arr){
+// for (let i = 0; i < arr.length; i++) {
+    
+//     arr[i]+=5;
+// }
+// return arr;
+// }
+
+// console.log(GetFive([6,5,5,5,22,1,33,45]));
+
+//#endregion
+
+
